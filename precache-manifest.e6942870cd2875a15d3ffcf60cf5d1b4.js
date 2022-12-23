9d0f5a1d92dd1e05b3048730ea519e0b57380ced
tyrusyeo <tyrusyeo5@gmail.com>
2022-12-23 10:36:27 -0500

    Updates

---
+++ precache-manifest.e6942870cd2875a15d3ffcf60cf5d1b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63fe9d0dc73dd59797048c57ade52d3",
+    "url": "/wolverineflicks/index.html"
+  },
+  {
+    "revision": "ae9ec250871921d65d9d",
+    "url": "/wolverineflicks/static/css/main.dddafa31.chunk.css"
+  },
+  {
+    "revision": "6dcd3dcd318e613757d8",
+    "url": "/wolverineflicks/static/js/2.d0ebc572.chunk.js"
+  },
+  {
+    "revision": "51ef5405210ac37567aba30ff34b026a",
+    "url": "/wolverineflicks/static/js/2.d0ebc572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9ec250871921d65d9d",
+    "url": "/wolverineflicks/static/js/main.1729c9ad.chunk.js"
+  },
+  {
+    "revision": "a688b1523ce77b4c8347",
+    "url": "/wolverineflicks/static/js/runtime-main.e009ed69.js"
+  }
+]);
